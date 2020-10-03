1185e0864dd12f4302355b9dea2d59e5f87a67bf
fabricio-davila <72274534+fabricio-davila@users.noreply.github.com>
2020-10-03 15:11:08 -0500

    Delete ComunidadRepository.java

--- ComunidadRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.icons.models.repositories;
-
-import java.util.List;
-
-import pe.edu.upc.icons.models.entities.Comunidad;
-
-public interface ComunidadRepository extends JpaRepository<Comunidad, Integer> {
-	List<Comunidad> findByNombre(String nombre) throws Exception;
-}
